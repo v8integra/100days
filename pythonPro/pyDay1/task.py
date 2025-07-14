35cb9b0955c71ae80cb75b31e1b7b628a6215a17
unknown <keith.francis83@gmail.com>
2025-07-13 20:42:50 -0600

    python day 1 task

---
+++ pythonPro/pyDay1/task.py
@@ -0,0 +1,6 @@
+# Band name generrator
+# Combines city name and pet name
+print("Welcome to the band name generator")
+city_name = input("What city did you grow up in?\n")
+pet_name = input("What was te name of your first pet?\n")
+print("The name of your band is: " + city_name + " " + pet_name + "!")
