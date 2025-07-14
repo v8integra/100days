b6ded264fcaa1b041e106fd05f5b3e8a0c3cd6da
unknown <keith.francis83@gmail.com>
2025-07-13 21:58:08 -0600

    added second python daily task

--- python/pyDay1/task1.py
+++ python/pyDay1/task1.py
@@ -3,4 +3,4 @@
 print("Welcome to the band name generator")
 city_name = input("What city did you grow up in?\n")
 pet_name = input("What was te name of your first pet?\n")
-print("The name of your band is: " + city_name + " " + pet_name + "!")
+print(f"The name of your band is: {city_name} {pet_name}!")
---
+++ python/pyDay1/task2.py
@@ -0,0 +1,8 @@
+# Welcome Message
+name = input("What is your name?\n")
+hobby = input("What is your hobby?\n")
+print("\n --- Welcome Message ---")
+print(f"Hello {name}!")
+print("Welcome to the world of Python Programming.")
+print(f"It's great to know that you love {hobby}")
+print("Get ready to build something amazing today.")
