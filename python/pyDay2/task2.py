6f7b3048a1c16030d776d061d4b08f6ea431371b
unknown <keith.francis83@gmail.com>
2025-07-14 20:44:42 -0600

    add day 2 python task

---
+++ python/pyDay2/task2.py
@@ -0,0 +1,10 @@
+# Personalized Greeting Program
+
+name = input("What is your name?\n")
+age = int(input("What is your age?\n"))
+color = input("What is your favorite color?\n")
+
+print("\n--- Personalized Greeting ---")
+print(f"Hello {name}!")
+print(f"You are {age} years old and {color} is a beautiful color!")
+print("You're now ready to start your Python adventure!")
