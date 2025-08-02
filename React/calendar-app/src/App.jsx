91aa33ebabb9e8eb95e2f51b1105012e689505e4
unknown <keith.francis83@gmail.com>
2025-08-02 16:57:37 -0600

    Calendar app jsx setup

---
+++ React/calendar-app/src/App.jsx
@@ -0,0 +1,12 @@
+import CalendarApp from "./components/calendarApp";
+import './Components/CalendarApp.css';
+
+const App = () => {
+  return (
+    <div className='container'>
+      <CalendarApp/>
+    </div>
+  )
+}
+
+export default App
